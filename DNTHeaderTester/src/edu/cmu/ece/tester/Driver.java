e96dd8bbd8f4dc6ef9790a9caf83f928918000d6
ylang <ylang@andrew.cmu.edu>
2013-03-03 22:23:44 -0500

    get suggest

--- DNTHeaderTester/src/edu/cmu/ece/tester/Driver.java
+++ DNTHeaderTester/src/edu/cmu/ece/tester/Driver.java
@@ -1,31 +1,42 @@
 package edu.cmu.ece.tester;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Driver {
 	public static void main(String[] args) {
-		// Create a new instance of the html unit driver
-		// Notice that the remainder of the code relies on the interface,
-		// not the implementation.
-		WebDriver driver = new HtmlUnitDriver();
+		// The Firefox driver supports javascript
+		WebDriver driver = new FirefoxDriver();
 
-		// And now use this to visit Google
-		driver.get("http://www.google.com");
+		// Go to the Google Suggest home page
+		driver.get("http://www.google.com/webhp?complete=1&hl=en");
 
-		// Find the text input element by its name
-		WebElement element = driver.findElement(By.name("q"));
+		// Enter the query string "Cheese"
+		WebElement query = driver.findElement(By.name("q"));
+		query.sendKeys("Cheese");
 
-		// Enter something to search for
-		element.sendKeys("Cheese!");
+		// Sleep until the div we want is visible or 5 seconds is over
+		long end = System.currentTimeMillis() + 5000;
+		while (System.currentTimeMillis() < end) {
+			WebElement resultsDiv = driver.findElement(By.className("gssb_e"));
 
-		// Now submit the form. WebDriver will find the form for us from the
-		// element
-		element.submit();
+			// If results have been returned, the results are displayed in a
+			// drop down.
+			if (resultsDiv.isDisplayed()) {
+				break;
+			}
+		}
 
-		// Check the title of the page
-		System.out.println("Page title is: " + driver.getTitle());
+		// And now list the suggestions
+		List<WebElement> allSuggestions = driver.findElements(By
+				.xpath("//td[@class='gssb_a gbqfsf']"));
+
+		for (WebElement suggestion : allSuggestions) {
+			System.out.println(suggestion.getText());
+		}
 	}
 }
